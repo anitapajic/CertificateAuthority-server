c011c4fc30da4d57a2df66031e47e6195d4a326f
Anita Pajic <anitaapajic@gmail.com>
2023-04-02 18:11:36 +0200

    Initial commit

---
+++ IBTim19/src/main/java/com/example/IBTim19/IbTim19Application.java
@@ -0,0 +1,13 @@
+package com.example.IBTim19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbTim19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbTim19Application.class, args);
+	}
+
+}
---
+++ IBTim19/src/test/java/com/example/IBTim19/IbTim19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IBTim19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbTim19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
