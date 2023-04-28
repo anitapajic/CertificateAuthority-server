1c26edbc77a0ed2467b97423c8de8d65c24525b4
Tamara <dzambic.sv43.2020@uns.ac.rs>
2023-04-28 19:56:49 +0200

    Fix needed

--- IBTim19/src/main/java/com/example/IBTim19/controller/UserController.java
+++ IBTim19/src/main/java/com/example/IBTim19/controller/UserController.java
@@ -4,19 +4,12 @@ import com.example.IBTim19.model.Certificate;
 import com.example.IBTim19.model.CertificateStatus;
 import com.example.IBTim19.service.CertificateGenerator;
 import com.example.IBTim19.service.CertificateService;
-import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SignatureException;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
@@ -41,19 +34,22 @@ public class UserController {
         }
         try {
             Certificate cert = certificateService.findOneBySerialNumber(sn);
-            Certificate issuerCertificate = certificateService.findOneBySerialNumber(cert.issuer);
+            Certificate issuerCertificate = certificateService.findOneBySerialNumber(cert.getIssuer());
 
             X509Certificate certificate = certificateGenerator.readCertificateFromFile(String.format("%s/%s.crt", "crts", sn));
             X509Certificate issCertificate = certificateGenerator.readCertificateFromFile(String.format("%s/%s.crt", "crts", issuerCertificate.getSerialNumber()));
-            
-            certificate.verify(issCertificate.getPublicKey());
+
+//            certificate.verify(issCertificate.getPublicKey());
 
             if (cert.validTo.after(new Date()) && cert.getValidTo().before(issuerCertificate.getValidTo()) && issuerCertificate.getStatus().equals(CertificateStatus.Valid) && cert.getStatus().equals(CertificateStatus.Valid)) {
                 return new ResponseEntity<>("This certificate is valid!", HttpStatus.OK);
             }
-        } catch (CertificateException | InvalidKeyException | NoSuchAlgorithmException | NoSuchProviderException | SignatureException e) {
-            return new ResponseEntity<>("This certificate is not valid!", HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+//        catch (CertificateException | InvalidKeyException | NoSuchAlgorithmException | NoSuchProviderException | SignatureException e) {
+//            return new ResponseEntity<>("This certificate is not valid!", HttpStatus.BAD_REQUEST);
+//        }
 
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
